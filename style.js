ff1570fd3a7d8b519c07707b31de067faf906452
Dakwon Smith <dsmith@nyc.yearup.org>
2018-11-15 16:49:17 -0500

    Hilda and hilbert

---
+++ style.js
@@ -0,0 +1,13 @@
+// Make a request for a user with a given ID
+axios.get("http://fizal.me/pokeapi/api/v2/name/name.json")
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
