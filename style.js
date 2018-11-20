f095a1d01a9b910264f28e886aa56d70f802eaab
Dakwon Smith <dsmith@nyc.yearup.org>
2018-11-20 16:50:46 -0500

    step into Javascript

--- style.js
+++ style.js
@@ -1,3 +1,23 @@
+// Needs an query selector
+// document.querySelecter("column").style.opacity = 1;
+// // Needs a function
+// function yourChara(){
+//   document.getElementsByClassName("").style.opacity = "1";
+//   document.getElementsByClassName("column").style.opacity = "1";
+//   document.addEventListener("click", yourChara(){
+// });
+
+let trainerName = prompt("What is your name?");
+
+if (prompt = )
+
+
+
+
+
+
+
+
 // Make a request for a user with a given ID
 axios.get("http://fizal.me/pokeapi/api/v2/name/name.json")
   .then(function (response) {
