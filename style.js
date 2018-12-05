f59241b597bcfb2892d5d952ed55e38684d41e31
Dakwon Smith <dsmith@nyc.yearup.org>
2018-12-05 09:00:36 -0500

    added other pokemon

--- style.js
+++ style.js
@@ -27,27 +27,27 @@ class pokemans{
 }
 
 // Make a request for a user with a given ID
-axios.get("https://fizal.me/pokeapi/api/v2/name/darkrai.json")
+axios.get("https://fizal.me/pokeapi/api/v2/name/zoroark.json")
   .then(function (response) {
     // handle success
     console.log(response);
     //get chosen pokemons stats
     console.log(response.data)
     //create the pokemons stats and call where it comes from
-let darkrai = new pokemans (
+let zoroark = new pokemans (
   response.data.sprites.front_default,
   response.data.name,
   response.data.stats[5].base_stat,
   response.data.stats[4].base_stat,
   response.data.stats[3].base_stat,
-  response.data.abilities[0].ability
+  response.data.abilities[0].ability.name
 )
-document.getElementById('darkSprite').src = darkrai.sprite;
-document.getElementById('darkName').innerHTML = "Name: " + darkrai.name;
-document.getElementById('darkHp').innerHTML = "Hp: " + darkrai.health;
-document.getElementById('darkAttack').innerHTML = "Attack: " + darkrai.attack;
-document.getElementById('darkDefense').innerHTML = "Defense: " + darkrai.defense;
-document.getElementById('darkAbility').innerHTML = "Ability: " + darkrai.abilities;
+document.getElementById('zoroarkSprite').src = zoroark.sprite;
+document.getElementById('zoroarkName').innerHTML = "Name: " + zoroark.name;
+document.getElementById('zoroarkHp').innerHTML = "Hp: " + zoroark.health;
+document.getElementById('zoroarkAttack').innerHTML = "Attack: " + zoroark.attack;
+document.getElementById('zoroarkDefense').innerHTML = "Defense: " + zoroark.defense;
+document.getElementById('zoroarkAbility').innerHTML = "Ability: " + zoroark.abilities;
 
 })
   .catch(function (error) {
@@ -57,3 +57,64 @@ document.getElementById('darkAbility').innerHTML = "Ability: " + darkrai.abiliti
   .then(function () {
     // always executed
   });
+
+  axios.get("https://fizal.me/pokeapi/api/v2/name/ditto.json")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      //get chosen pokemons stats
+      console.log(response.data)
+      //create the pokemons stats and call where it comes from
+  let ditto = new pokemans (
+    response.data.sprites.front_default,
+    response.data.name,
+    response.data.stats[5].base_stat,
+    response.data.stats[4].base_stat,
+    response.data.stats[3].base_stat,
+    response.data.abilities[0].ability.name
+  )
+  document.getElementById('dittoSprite').src = ditto.sprite;
+  document.getElementById('dittoName').innerHTML = "Name: " + ditto.name;
+  document.getElementById('dittoHp').innerHTML = "Hp: " + ditto.health;
+  document.getElementById('dittoAttack').innerHTML = "Attack: " + ditto.attack;
+  document.getElementById('dittoDefense').innerHTML = "Defense: " + ditto.defense;
+  document.getElementById('dittoAbility').innerHTML = "Ability: " + ditto.abilities;
+
+  })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+    axios.get("https://fizal.me/pokeapi/api/v2/name/typhlosion.json")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        //get chosen pokemons stats
+        console.log(response.data)
+        //create the pokemons stats and call where it comes from
+    let typhlosion = new pokemans (
+      response.data.sprites.front_default,
+      response.data.name,
+      response.data.stats[5].base_stat,
+      response.data.stats[4].base_stat,
+      response.data.stats[3].base_stat,
+      response.data.abilities[0].ability.name
+    )
+    document.getElementById('typhloSprite').src = typhlosion.sprite;
+    document.getElementById('typhloName').innerHTML = "Name: " + typhlosion.name;
+    document.getElementById('typhloHp').innerHTML = "Hp: " + typhlosion.health;
+    document.getElementById('typhloAttack').innerHTML = "Attack: " + typhlosion.attack;
+    document.getElementById('typhloDefense').innerHTML = "Defense: " + typhlosion.defense;
+    document.getElementById('typhloAbility').innerHTML = "Ability: " + typhlosion.abilities;
+
+    })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
